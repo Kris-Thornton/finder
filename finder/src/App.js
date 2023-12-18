87cb9052374199ec9c7fc356c345fb5cd44427b0
Kris Thornton <Kristhornton@ymail.com>
2023-12-17 20:25:01 -0800

    updated title

--- finder/src/App.js
+++ finder/src/App.js
@@ -4,8 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className={'App-header'}>
+      <header className={'title-header'}>
+        <h1>FIND-</h1><h1 className={'title-backend'}>ER</h1>
+      </header>
+      
       <img src={logo} className="App-logo" alt="logo" />
-      <h1>Finder</h1>
     </div>
   )
 }
