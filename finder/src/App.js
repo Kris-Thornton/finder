fb3b6433cdb8ecefcb7d59c52fd021e922f093bf
Kris Thornton <Kristhornton@ymail.com>
2023-12-29 07:31:55 -0800

    updated Header

--- finder/src/App.js
+++ finder/src/App.js
@@ -1,13 +1,66 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import logo from './logo.svg';
 import './App.css';
-import Header from './header/header';
+import  Profile  from './components/profile';
+
+
+
+
+
+function Header()  {
+  return (
+      <>
+
+    
+
+
+       <section >
+
+        <div className={'App-header'}>
+          <header className={'title-header'}>
+            <h1>FIND-</h1><h1 className={'title-backend'}>ER</h1>
+          </header>
+    
+            <img src={logo} className="App-logo" alt="logo" />
+        
+
+
+          <div className={'header'}>
+              <Link to='/'>
+                  <h3 className={'headerWord'}>Home</h3>
+              </Link>
+              <Link to='/profile'>
+                  <h3 className={'headerWord'}>Profile</h3>
+              </Link>
+              
+              <h3 className={'headerWord'}>Account</h3>
+              <h3 className={'headerWord'}>Settings</h3>
+              <h3 className={'headerWord'}>Find-ER Map</h3>
+              
+          </div>
+      </div>
+          
+      </section>
+
+
+      
+      </>
+     
+  )
+  
+}
+
+
+
+
+
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Header />}>
-
+        <Route path='/profile' element={<Profile />} />
 
 
         </Route>
---
+++ finder/src/components/profile.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+function Profile() {
+    return (
+        <div className="'background'">
+            <h1>
+                Profile page
+            </h1>
+            
+        </div>
+    )
+}
+
+export default Profile;
