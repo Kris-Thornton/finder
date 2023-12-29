dd067e5711a013aeb38ffcceeed6a1d534ac176a
Kris Thornton <Kristhornton@ymail.com>
2023-12-29 15:51:58 -0800

    linking up routes

--- finder/src/App.js
+++ finder/src/App.js
@@ -1,57 +1,7 @@
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import logo from './logo.svg';
+import logoMain from './logoMain.svg.png'
 import './App.css';
-import  Profile  from './components/profile';
-
-
-
-
-
-function Header()  {
-  return (
-      <>
-
-    
-
-
-       <section >
-
-        <div className={'App-header'}>
-          <header className={'title-header'}>
-            <h1>FIND-</h1><h1 className={'title-backend'}>ER</h1>
-          </header>
-    
-            <img src={logo} className="App-logo" alt="logo" />
-        
-
-
-          <div className={'header'}>
-              <Link to='/'>
-                  <h3 className={'headerWord'}>Home</h3>
-              </Link>
-              <Link to='/profile'>
-                  <h3 className={'headerWord'}>Profile</h3>
-              </Link>
-              
-              <h3 className={'headerWord'}>Account</h3>
-              <h3 className={'headerWord'}>Settings</h3>
-              <h3 className={'headerWord'}>Find-ER Map</h3>
-              
-          </div>
-      </div>
-          
-      </section>
-
-
-      
-      </>
-     
-  )
-  
-}
-
-
-
+import Profile from './components/profile';
 
 
 
@@ -59,14 +9,59 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Header />}>
-        <Route path='/profile' element={<Profile />} />
+        <Route path="/" element={ <NavBar />} /> 
+          <Route path='/profile' element={ <Profile />} />
 
 
-        </Route>
+        
       </Routes>
     </BrowserRouter>
   )
 }
+
+
+function NavBar(props) {
+  return (
+    <>
+      <section >
+    {/* FIND-ER Name and logo section in header */}
+        <div className={'App-header'}>
+          <header className={'title-header'}>
+            <h1>FIND-</h1><h1 className={'title-backend'}>ER</h1>
+          </header>
+        <img src={logoMain} className="App-logo" alt="logo" />
+
+
+    {/* Navigation Bar section */}
+          <div className={'header'}>
+            <Link to='/'>
+              <h3 className={'headerWord'}>Home</h3>
+            </Link>
+            <Link to='/profile'>
+              <h3 className={'headerWord'}>Profile</h3>
+            </Link>
+
+            <h3 className={'headerWord'}>Account</h3>
+            <h3 className={'headerWord'}>Settings</h3>
+            <h3 className={'headerWord'}>Find-ER Map</h3>
+
+          </div>
+        </div>
+
+      </section>
+
+
+
+    </>
+
+  )
+
+}
+
+
+
+
+
+
 
 export default App;
--- finder/src/components/profile.js
+++ finder/src/components/profile.js
@@ -3,10 +3,22 @@ import React from "react";
 
 function Profile() {
     return (
-        <div className="'background'">
+        <div className="background">
             <h1>
                 Profile page
             </h1>
+            <p>
+                dfasdfdsfdasfasdfasdfsdfdsfeefwadfadfaeawfdsfasdfasdf
+                sdfasdfdsfasdf
+                asdfasfeafsdf
+                asdfasdfasdfasefadf
+                asdfdasfaseffds
+                asfesfasdfaseterwfdsgffdghsafas
+                asdfaedfewfwafwgdfgasdfas
+                aseawetasfasdfawefas
+                adefaewgaseawefasd
+                asfeaewsgaseawf
+            </p>
             
         </div>
     )
